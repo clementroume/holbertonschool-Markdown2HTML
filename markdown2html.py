213e02dbc955564b69cd17785bc4b2e474dfd16e
Clément Roume <clement.roume@hotmail.com>
2024-12-09 17:44:23 +0100

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -14,23 +14,24 @@ def main():
 
     # Ensure at least 3 arguments are provided: script name, input file,
     # and output file
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         # Print usage error message to stderr
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         # Exit with an error code 1
-        sys.exit(1)
+        exit(1)
 
     # Extract the input file name from command-line arguments
     input_file = sys.argv[1]
     # Check if the specified input file exists
     if not os.path.isfile(input_file):
         # Print a missing file error message to stderr
-        sys.stderr.write(f"Missing {input_file}\n")
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         # Exit with an error code 1
-        sys.exit(1)
+        exit(1)
 
     # If all checks pass, the script exits successfully
-    sys.exit(0)
+    exit(0)
 
 
 if __name__ == "__main__":
