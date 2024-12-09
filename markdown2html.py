cc1af2e2f03f9873dfd8075aff5f38d6fca0d117
Clément Roume <clement.roume@hotmail.com>
2024-12-09 17:19:02 +0100

    Start a script

---
+++ markdown2html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+if (len(sys.argv) < 3):
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+    exit(1)
+
+if not (os.path.isfile(sys.argv[1])):
+    sys.stderr.write("Missing " + sys.argv[1])
+    exit(1)
+
+exit(0)
